60257823e2148d11ae7a6afe966c21695ff9c230
Andrew Davidson <acdavidson@sbcglobal.net>
2015-03-20 19:04:45 -0400

    Finished Tasks 6 and 7: writing test cases

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,124 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+
+
+
+
+      
+        [Test()]
+        public
+        void
+        TestThatFlightInitializes
+        ()
+        {
+            var
+            target =
+            new
+            Flight(new DateTime(2015, 4, 18), new DateTime(2015, 4, 29), 700);
+            Assert.IsNotNull(target);
+        }
+
+       
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public
+        void
+        TestThatFlightThrowsOnBadDates
+        
+        ()
+        {
+            new
+            Flight
+            ( new DateTime(2015, 4, 29), new DateTime(2015, 4, 18), 700);
+        }
+
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public
+        void
+        TestThatFlightThrowsOnBadMiles
+        ()
+        {
+            new
+            Flight
+            (new DateTime(2015, 4, 18), new DateTime(2015, 4, 29), -5);
+        }
+
+
+
+        [Test()]
+        public
+        void
+        TestThatFlightHasCorrectBasePriceForSameDayFlight
+        ()
+        {
+            var
+            target =
+            new
+            Flight(new DateTime(2015, 4, 18), new DateTime(2015, 4, 18), 700);
+            Assert.AreEqual(
+            200
+            , target.getBasePrice());
+        }
+
+
+        [Test()]
+        public
+        void
+        TestThatFlightHasCorrectBasePriceForOneDayFlightGap
+        ()
+        {
+            var
+            target =
+            new
+            Flight(new DateTime(2015, 4, 18), new DateTime(2015, 4, 19), 700);
+            Assert.AreEqual(
+            220
+            , target.getBasePrice());
+        }
+
+
+
+        [Test()]
+        public
+        void
+        TestThatFlightHasCorrectBasePriceForTwoDayFlightGap
+        ()
+        {
+            var
+            target =
+            new
+            Flight(new DateTime(2015, 4, 18), new DateTime(2015, 4, 20), 700);
+            Assert.AreEqual(
+            240
+            , target.getBasePrice());
+        }
+
+
+        [Test()]
+        public
+        void
+        TestThatFlightHasCorrectBasePriceForLongFlightGap
+        ()
+        {
+            var
+            target =
+            new
+            Flight(new DateTime(2015, 4, 18), new DateTime(2018, 9, 30), 700);
+            Assert.AreEqual(
+            25420
+            , target.getBasePrice());
+        }
+
+
+
+
+
+
+
+
 	}
 }
